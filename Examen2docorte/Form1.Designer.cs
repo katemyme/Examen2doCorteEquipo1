bf05328af8ff3052e72cc43064ceab089349f083
ValcocerLopez <vmalcocer@uamv.edu.ni>
2025-10-27 10:33:28 -0600

    listo todo

--- Examen2docorte/Form1.Designer.cs
+++ Examen2docorte/Form1.Designer.cs
@@ -30,122 +30,135 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tbNormal = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.helpProvider1 = new System.Windows.Forms.HelpProvider();
             this.label4 = new System.Windows.Forms.Label();
-            this.tbEmergencia = new System.Windows.Forms.TextBox();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.listBox2 = new System.Windows.Forms.ListBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.tbCritico = new System.Windows.Forms.TextBox();
+            this.lbNormal = new System.Windows.Forms.ListBox();
+            this.lbCritico = new System.Windows.Forms.ListBox();
+            this.btnEliminarPacienteNormal = new System.Windows.Forms.Button();
+            this.btnEliminarPacienteCritico = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(36, 9);
+            this.label1.Location = new System.Drawing.Point(27, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(67, 16);
+            this.label1.Size = new System.Drawing.Size(54, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Pacientes";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(138, 9);
+            this.label2.Location = new System.Drawing.Point(150, 7);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(150, 16);
+            this.label2.Size = new System.Drawing.Size(118, 13);
             this.label2.TabIndex = 1;
             this.label2.Text = "Pacientes Emergencias";
             // 
-            // textBox1
+            // tbNormal
             // 
-            this.textBox1.Location = new System.Drawing.Point(13, 55);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 2;
+            this.tbNormal.Location = new System.Drawing.Point(30, 45);
+            this.tbNormal.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbNormal.Name = "tbNormal";
+            this.tbNormal.Size = new System.Drawing.Size(76, 20);
+            this.tbNormal.TabIndex = 2;
+            this.tbNormal.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbNormal_KeyDown);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(36, 36);
+            this.label3.Location = new System.Drawing.Point(40, 29);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(39, 16);
+            this.label3.Size = new System.Drawing.Size(31, 13);
             this.label3.TabIndex = 3;
             this.label3.Text = "Caso";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(217, 36);
+            this.label4.Location = new System.Drawing.Point(163, 29);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(39, 16);
+            this.label4.Size = new System.Drawing.Size(31, 13);
             this.label4.TabIndex = 4;
             this.label4.Text = "Caso";
             // 
-            // tbEmergencia
+            // tbCritico
             // 
-            this.tbEmergencia.Location = new System.Drawing.Point(176, 55);
-            this.tbEmergencia.Name = "tbEmergencia";
-            this.tbEmergencia.Size = new System.Drawing.Size(100, 22);
-            this.tbEmergencia.TabIndex = 5;
-            this.tbEmergencia.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyDown);
+            this.tbCritico.Location = new System.Drawing.Point(166, 44);
+            this.tbCritico.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbCritico.Name = "tbCritico";
+            this.tbCritico.Size = new System.Drawing.Size(76, 20);
+            this.tbCritico.TabIndex = 5;
+            this.tbCritico.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbCritico_KeyDown);
             // 
-            // listBox1
+            // lbNormal
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(13, 134);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(120, 244);
-            this.listBox1.TabIndex = 6;
+            this.lbNormal.FormattingEnabled = true;
+            this.lbNormal.Location = new System.Drawing.Point(30, 109);
+            this.lbNormal.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.lbNormal.Name = "lbNormal";
+            this.lbNormal.Size = new System.Drawing.Size(91, 238);
+            this.lbNormal.TabIndex = 6;
             // 
-            // listBox2
+            // lbCritico
             // 
-            this.listBox2.FormattingEnabled = true;
-            this.listBox2.ItemHeight = 16;
-            this.listBox2.Location = new System.Drawing.Point(190, 134);
-            this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(120, 244);
-            this.listBox2.TabIndex = 7;
+            this.lbCritico.FormattingEnabled = true;
+            this.lbCritico.Location = new System.Drawing.Point(166, 109);
+            this.lbCritico.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.lbCritico.Name = "lbCritico";
+            this.lbCritico.Size = new System.Drawing.Size(91, 238);
+            this.lbCritico.TabIndex = 7;
             // 
-            // button1
+            // btnEliminarPacienteNormal
             // 
-            this.button1.Location = new System.Drawing.Point(-9, 83);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(159, 23);
-            this.button1.TabIndex = 8;
-            this.button1.Text = "Eliinar Paciente Comun";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnEliminarPacienteNormal.Location = new System.Drawing.Point(10, 69);
+            this.btnEliminarPacienteNormal.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnEliminarPacienteNormal.Name = "btnEliminarPacienteNormal";
+            this.btnEliminarPacienteNormal.Size = new System.Drawing.Size(119, 36);
+            this.btnEliminarPacienteNormal.TabIndex = 8;
+            this.btnEliminarPacienteNormal.Text = "Eliminar Paciente Comun";
+            this.btnEliminarPacienteNormal.UseVisualStyleBackColor = true;
+            this.btnEliminarPacienteNormal.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // btnEliminarPacienteCritico
             // 
-            this.button2.Location = new System.Drawing.Point(176, 83);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(193, 23);
-            this.button2.TabIndex = 9;
-            this.button2.Text = "Eliinar Paciente Emergencia";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.btnEliminarPacienteCritico.Location = new System.Drawing.Point(153, 69);
+            this.btnEliminarPacienteCritico.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnEliminarPacienteCritico.Name = "btnEliminarPacienteCritico";
+            this.btnEliminarPacienteCritico.Size = new System.Drawing.Size(115, 36);
+            this.btnEliminarPacienteCritico.TabIndex = 9;
+            this.btnEliminarPacienteCritico.Text = "Eliminar Paciente Emergencia";
+            this.btnEliminarPacienteCritico.UseVisualStyleBackColor = true;
+            this.btnEliminarPacienteCritico.Click += new System.EventHandler(this.btnEliminarPacienteCritico_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.listBox2);
-            this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.tbEmergencia);
+            this.ClientSize = new System.Drawing.Size(363, 366);
+            this.Controls.Add(this.btnEliminarPacienteCritico);
+            this.Controls.Add(this.btnEliminarPacienteNormal);
+            this.Controls.Add(this.lbCritico);
+            this.Controls.Add(this.lbNormal);
+            this.Controls.Add(this.tbCritico);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tbNormal);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -155,15 +168,15 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox tbNormal;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.HelpProvider helpProvider1;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox tbEmergencia;
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.ListBox listBox2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox tbCritico;
+        private System.Windows.Forms.ListBox lbNormal;
+        private System.Windows.Forms.ListBox lbCritico;
+        private System.Windows.Forms.Button btnEliminarPacienteNormal;
+        private System.Windows.Forms.Button btnEliminarPacienteCritico;
     }
 }
 
--- Examen2docorte/Form1.cs
+++ Examen2docorte/Form1.cs
@@ -10,45 +10,76 @@ using System.Windows.Forms;
 
 namespace Examen2docorte
 {
-
-
     public partial class Form1 : Form
     {
-
-
+        Queue<string> Pacientes = new Queue<string>();
         Stack<string> Pacienteurgente = new Stack<string>();
-
-
         public Form1()
         {
             InitializeComponent();
         }
-
-        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Pacientes.Count > 0)
+            {
+                string pacienteAtendido = Pacientes.Dequeue();
+                MessageBox.Show("Paciente atendido: " + pacienteAtendido);
+                MostrarPacientes();
+            }
+            else
+            {
+                MessageBox.Show("No hay pacientes en espera.");
+            }
+        }
+        private void tbNormal_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.Enter)
             {
-                Pacienteurgente.Push(tbEmergencia.Text);
-                tbEmergencia.Clear();
+                e.SuppressKeyPress = true;
+                Pacientes.Enqueue(tbNormal.Text);
+                tbNormal.Clear();
+                tbNormal.Focus();
+                MostrarPacientes();
+            }
+        }
+        private void MostrarPacientes()
+        {
+            lbNormal.Items.Clear();
+            foreach (var paciente in Pacientes)
+            {
+                lbNormal.Items.Add(paciente);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+       
+        private void MostrarPila()
+        {
+            lbCritico.Items.Clear();
+            foreach (string paciente in Pacienteurgente)
+            {
+                lbCritico.Items.Add(paciente);
+
+            }
+            tbCritico.Clear();
+        }
+
+        private void tbCritico_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                Pacienteurgente.Push(tbCritico.Text);
+                tbCritico.Clear();
+                tbCritico.Focus();
                 MostrarPila();
             }
         }
 
-
-        private void MostrarPila()
-        {
-            listBox1.Items.Clear();
-            foreach (string paciente in Pacienteurgente)
-            {
-                listBox2.Items.Add(paciente);
-                
-            }
-            tbEmergencia.Clear();
-            
-
-        }
-
-        private void button2_Click(object sender, EventArgs e)
+        private void btnEliminarPacienteCritico_Click(object sender, EventArgs e)
         {
 
             if (Pacienteurgente.Count > 0)
@@ -63,6 +94,5 @@ namespace Examen2docorte
         }
     }
 
-
-
 }
+
